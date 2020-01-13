5bdf813ccf9b3ade0625e6d2d6d5e7c0ce992f9d
1stVyacheslav <komaroff4@gmail.com>
2020-01-13 12:01:31 +0500

    cherry-pick from newBranch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import App from './app';
+import './style.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const App = ( {name} ) => <div className='hello'>Hello {name} !</div>
+
+ReactDOM.render( <App name="world" />, document.getElementById("root") )
+
+// import Calc from './calc'
+// import Log from './log'
+
+// import icon from './reactjs.png'
+
+// const calc = new Calc(),
+// 			log = new Log()
+
+// log.print(calc.sum(1, 8, 40, 5))
+
+// const rootEl = document.getElementById('root');
+// const img = document.createElement('img');
+// img.src = `dist/${icon}`;
+
+// rootEl.appendChild(img)
--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,61 @@
 module.exports = {
 	mode: 'development',
 
-	devServer: {
-    contentBase: './dist'
-	},
-	
-	devtool: 'inline-source-map',
-
 	module: {
 		rules: [
+
+			//Loading scripts
 			{
-				test: /\.m?js$/,
-				exclude: /(node_modules)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-react', '@babel/preset-env']
-					}
-				}
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: [ {loader: 'babel-loader'} ]
 			},
+
+			//Loading CSS
 			{
 				test: /\.css$/,
-				use: ['style-loader', 'css-loader']
+				use: [ 
+					{loader: 'style-loader'}, 
+					{loader: 'css-loader'} 
+				]
+			},
+
+			//Loading images
+			{
+				test: /\.(png|jpe?g|gif|ico)$/,
+				use: [ {
+						loader: 'file-loader',
+						options: {
+							outputPath: 'images',
+							name: '[name]-[sha1:hash:7].[ext]'
+						}
+					} ]
 			}
+
 		]
 	}
+
+
+	// devServer: {
+  //   contentBase: './dist'
+	// },
 	
+	// devtool: 'inline-source-map',
+
+	// rules: [
+  //   {
+  //     test: /\.m?js$/,
+  //     exclude: /(node_modules)/,
+  //     use: {
+  //       loader: 'babel-loader',
+  //       options: {
+  //         presets: ['@babel/preset-env', '@babel/preset-react']
+  //       }
+  //     }
+	// 	},
+	// 	{
+	// 		test: /\.css$/,
+	// 		use: ['style-loader', 'css-loader']
+	// 	}
+  // ]
 }
